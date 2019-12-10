102fdb22eceb71ec92a9a2e723f50373833e2eb8
Vladislav Yundin <4yundin@gmail.com>
2019-12-10 19:01:30 +0300

    Redundant casts removed from MainActivity. Imports optimized.

--- app/src/main/java/com/example/hookahapp/MainActivity.java
+++ app/src/main/java/com/example/hookahapp/MainActivity.java
@@ -1,25 +1,15 @@
 package com.example.hookahapp;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.viewpager.widget.ViewPager;
 
-import android.content.Context;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.TabHost;
-import android.widget.TextView;
 import com.arellomobile.mvp.MvpAppCompatActivity;
 import com.arellomobile.mvp.presenter.InjectPresenter;
 
-
-import javax.inject.Inject;
-
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import toothpick.Scope;
-import toothpick.Toothpick;
 
 
 public class MainActivity extends MvpAppCompatActivity implements MainActivityVew{
@@ -40,10 +30,10 @@ public class MainActivity extends MvpAppCompatActivity implements MainActivityVe
 //        Scope appScope = Toothpick.openScope(this);
 //        Toothpick.inject(this, appScope);
         setContentView(R.layout.activity_main);
-        ViewPager viewPager = (ViewPager)findViewById(R.id.viewPager);
+        ViewPager viewPager = findViewById(R.id.viewPager);
         menuPageAdapter = new MenuPageAdapter(getSupportFragmentManager(), getApplicationContext());
         viewPager.setAdapter(menuPageAdapter);
-        TextView deal = (TextView) findViewById(R.id.deal);
+        TextView deal = findViewById(R.id.deal);
         deal.setOnClickListener(v -> {menuPageAdapter.getItem(1);
             deal.setTag(deal.getBackground());
             deal.setBackground(getApplicationContext().getResources().getDrawable(R.drawable.menu_rectangle));});
@@ -52,7 +42,7 @@ public class MainActivity extends MvpAppCompatActivity implements MainActivityVe
 
     @OnClick(R.id.deal)
     void dealButtonClicked(){
-        TextView deal = (TextView) findViewById(R.id.deal);
+        TextView deal = findViewById(R.id.deal);
         deal.setTag(deal.getBackground());
         deal.setBackground(getApplicationContext().getResources().getDrawable(R.drawable.menu_rectangle));
         menuPageAdapter.getItem(1);
