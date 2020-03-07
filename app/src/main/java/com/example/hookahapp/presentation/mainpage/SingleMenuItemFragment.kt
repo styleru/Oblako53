3181bdd2426caa1d48c557da1d3ed53968603cd1
Vladislav Yundin <4yundin@gmail.com>
2020-03-07 23:15:10 +0300

    Expanding BottomSheetDialogFragment to full height

--- app/src/main/java/com/example/hookahapp/presentation/mainpage/SingleMenuItemFragment.kt
+++ app/src/main/java/com/example/hookahapp/presentation/mainpage/SingleMenuItemFragment.kt
@@ -5,11 +5,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import com.example.hookahapp.R
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.android.synthetic.main.single_menu_item_fragment.*
+
 
 class SingleMenuItemFragment : BottomSheetDialogFragment() {
     companion object{
@@ -53,6 +57,14 @@ class SingleMenuItemFragment : BottomSheetDialogFragment() {
             else -> throw Exception("Unknown key")
         }
         super.onViewCreated(view, savedInstanceState)
+
+        view.viewTreeObserver.addOnGlobalLayoutListener {
+            val dialog = dialog as BottomSheetDialog?
+            val bottomSheet = dialog!!.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet) as FrameLayout
+            val behavior: BottomSheetBehavior<*> = BottomSheetBehavior.from<View>(bottomSheet)
+            behavior.state = BottomSheetBehavior.STATE_EXPANDED
+            behavior.peekHeight = 0
+        }
     }
 
     override fun onAttach(context: Context) {
